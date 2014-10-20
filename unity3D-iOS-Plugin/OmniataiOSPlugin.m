46148a92bcc2c34eda5537d94de9b05b88806226
Jun <jun.liu@omniata.com>
2014-10-20 10:07:37 +0300

    iOS plugins and example project

---
+++ unity3D-iOS-Plugin/OmniataiOSPlugin.m
@@ -0,0 +1,34 @@
+#import "iOmniataAPI.h"
+// Converts C style string to NSString
+#define GetStringParam( _x_ ) ( _x_ != NULL ) ? [NSString stringWithUTF8String:_x_] : [NSString stringWithUTF8String:""]
+
+extern void Initialize(const char* api_key, const char* user_id, bool debug)
+{
+	[iOmniataAPI initializeWithApiKey:GetStringParam(api_key) UserId:GetStringParam(user_id) AndDebug:debug];
+}
+
+extern void TrackPurchaseEvent(const double total, const char* currency_code)
+{
+	[iOmniataAPI trackPurchaseEvent:total currency_code: GetStringParam(currency_code)];
+}
+
+extern void TrackEvent(const char* type,const char *parameters) {
+    NSString *attris = GetStringParam(parameters);
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    NSMutableDictionary *paraDict = [[NSMutableDictionary alloc] init];
+    for (int i=0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [paraDict setObject:value forKey:key];
+        }
+    }
+    [iOmniataAPI trackEvent: GetStringParam(type):paraDict];
+}
+
+extern void TrackLoad()
+{
+    [iOmniataAPI trackLoadEvent];
+}
